223e815d171db13a2985c76106560dcaaf8a1313
Mark Rushakoff <mark.rushakoff@gmail.com>
2024-04-23 08:28:45 -0400

    feat: add DropDuplicateFeedbackMapper
    The AcceptAllValidFeedbackMapper was a trivial starting point, but
    DropDuplicateFeedbackMapper seems to run better in tests with many
    validators, to avoid propagating duplicative messages through the
    network.

--- tm/tmconsensus/feedbackmapper.go
+++ tm/tmconsensus/feedbackmapper.go
@@ -71,3 +71,68 @@ func (m AcceptAllValidFeedbackMapper) mapVoteResult(
 		panic(fmt.Errorf("BUG: no %s mapping set for %s", name, f))
 	}
 }
+
+// DropDuplicateFeedbackMapper is a [Handler] that wraps a FineGrainedConsensusHandler
+// that ignores proposed block messages if we already have the proposed block
+// and ignores vote messages if they do not increase existing vote knowledge.
+type DropDuplicateFeedbackMapper struct {
+	Handler FineGrainedConsensusHandler
+}
+
+func (m DropDuplicateFeedbackMapper) HandleProposedBlock(
+	ctx context.Context, pb ProposedBlock,
+) gexchange.Feedback {
+	f := m.Handler.HandleProposedBlock(ctx, pb)
+	switch f {
+	case HandleProposedBlockAccepted:
+		return gexchange.FeedbackAccepted
+
+	case HandleProposedBlockRoundTooOld,
+		HandleProposedBlockInternalError,
+		HandleProposedBlockAlreadyStored:
+		return gexchange.FeedbackIgnored
+
+	case HandleProposedBlockSignerUnrecognized,
+		HandleProposedBlockBadSignature,
+		HandleProposedBlockBadBlockHash:
+		return gexchange.FeedbackRejected
+
+	default:
+		panic(fmt.Errorf("BUG: no HandleProposedBlockResult mapping set for %s", f))
+	}
+}
+
+func (m DropDuplicateFeedbackMapper) HandlePrevoteProofs(
+	ctx context.Context, p PrevoteSparseProof,
+) gexchange.Feedback {
+	f := m.Handler.HandlePrevoteProofs(ctx, p)
+	return m.mapVoteResult(f, "HandlePrevoteProofs")
+}
+
+func (m DropDuplicateFeedbackMapper) HandlePrecommitProofs(
+	ctx context.Context, p PrecommitSparseProof,
+) gexchange.Feedback {
+	f := m.Handler.HandlePrecommitProofs(ctx, p)
+	return m.mapVoteResult(f, "HandlePrecommitProofs")
+}
+
+func (m DropDuplicateFeedbackMapper) mapVoteResult(
+	f HandleVoteProofsResult, name string,
+) gexchange.Feedback {
+	switch f {
+	case HandleVoteProofsAccepted:
+		return gexchange.FeedbackAccepted
+
+	case HandleVoteProofsRoundTooOld,
+		HandleVoteProofsNoNewSignatures,
+		HandleVoteProofsInternalError:
+		return gexchange.FeedbackIgnored
+
+	case HandleVoteProofsEmpty,
+		HandleVoteProofsBadPubKeyHash:
+		return gexchange.FeedbackRejected
+
+	default:
+		panic(fmt.Errorf("BUG: no %s mapping set for %s", name, f))
+	}
+}
