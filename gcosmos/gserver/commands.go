bd2384cd33767b9d7a90919cda48a1bb4d330f21
Mark Rushakoff <mark.rushakoff@gmail.com>
2024-08-16 15:47:11 -0400

    feat: add val-pub-key gordian subcommand
    This prints the validator public key in the format we need for "tx
    staking create-validator". Although the output there suggests using the
    tendermint subcommand, it isn't available on simapp for some reason. So,
    we just toss it under gordian for now.

---
+++ gcosmos/gserver/commands.go
@@ -0,0 +1,105 @@
+package gserver
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/cometbft/cometbft/privval"
+	"github.com/cosmos/cosmos-sdk/client"
+	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
+	"github.com/libp2p/go-libp2p"
+	dht "github.com/libp2p/go-libp2p-kad-dht"
+	libp2phost "github.com/libp2p/go-libp2p/core/host"
+	libp2ppeer "github.com/libp2p/go-libp2p/core/peer"
+	"github.com/rollchains/gordian/tm/tmp2p/tmlibp2p"
+	"github.com/spf13/cobra"
+)
+
+func newSeedCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "seed",
+		Short: `Run a "seed node" as a central discovery point for other libp2p nodes`,
+		Args:  cobra.ExactArgs(1),
+
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx, cancel := context.WithCancel(cmd.Context())
+			defer cancel()
+
+			h, err := tmlibp2p.NewHost(
+				ctx,
+				tmlibp2p.HostOptions{
+					Options: []libp2p.Option{
+						// Since unspecified, use a dynamic identity and a random listening address.
+						// Specify only tcp for the protocol, just for simplicity in stack traces.
+						libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0"),
+
+						// Unsure if this is something we always want.
+						// Can be controlled by a flag later if undesirable by default.
+						libp2p.ForceReachabilityPublic(),
+					},
+				},
+			)
+			if err != nil {
+				return fmt.Errorf("failed to create libp2p host: %w", err)
+			}
+
+			host := h.Libp2pHost()
+
+			// We currently rely on the libp2p DHT for peer discovery,
+			// so the seed node needs to run the DHT as well.
+			// The validators connect to the DHT through the tmlibp2p.Connection type.
+			if _, err := dht.New(ctx, host, dht.ProtocolPrefix("/gordian")); err != nil {
+				return fmt.Errorf("failed to create DHT peer for seed: %w", err)
+			}
+
+			hostInfo := libp2phost.InfoFromHost(host)
+			p2pAddrs, err := libp2ppeer.AddrInfoToP2pAddrs(hostInfo)
+			if err != nil {
+				return fmt.Errorf("failed to get host p2p addrs: %w", err)
+			}
+
+			var hostAddrs []string
+			for _, a := range p2pAddrs {
+				hostAddrs = append(hostAddrs, a.String())
+			}
+
+			joinedAddrs := strings.Join(hostAddrs, "\n") + "\n" // Trailing newline indicates proper end of input.
+
+			if err := os.WriteFile(args[0], []byte(joinedAddrs), 0600); err != nil {
+				return fmt.Errorf("failed to write seed address output file %q: %w", args[0], err)
+			}
+
+			fmt.Fprintf(cmd.ErrOrStderr(), "Seed running at multiaddrs: %s\n", joinedAddrs)
+
+			return nil
+		},
+	}
+}
+
+func newPrintValPubKeyCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "val-pub-key",
+		Short: `Print the JSON for the validator public key, suitable to use in the pubkey field of the create-validator JSON`,
+		Args:  cobra.NoArgs,
+
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cometConfig := client.GetConfigFromCmd(cmd)
+			fpv := privval.LoadFilePV(cometConfig.PrivValidatorKeyFile(), cometConfig.PrivValidatorStateFile())
+
+			sdkPK, err := cryptocodec.FromCmtPubKeyInterface(fpv.Key.PubKey)
+			if err != nil {
+				return fmt.Errorf("failed to extract SDK public key: %w", err)
+			}
+
+			clientCtx := client.GetClientContextFromCmd(cmd)
+			j, err := clientCtx.Codec.MarshalInterfaceJSON(sdkPK)
+			if err != nil {
+				return fmt.Errorf("failed to marshal SDK key to JSON: %w", err)
+			}
+			fmt.Fprintln(cmd.OutOrStdout(), string(j))
+			return nil
+		},
+	}
+}
--- gcosmos/gserver/component.go
+++ gcosmos/gserver/component.go
@@ -19,8 +19,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/libp2p/go-libp2p"
-	dht "github.com/libp2p/go-libp2p-kad-dht"
-	libp2phost "github.com/libp2p/go-libp2p/core/host"
 	libp2ppeer "github.com/libp2p/go-libp2p/core/peer"
 	"github.com/rollchains/gordian/gcosmos/gserver/internal/gsbd"
 	"github.com/rollchains/gordian/gcosmos/gserver/internal/gsi"
@@ -434,67 +432,10 @@ func (c *Component) WriteCustomConfigAt(configPath string) error {
 
 func (c *Component) CLICommands() serverv2.CLIConfig {
 	return serverv2.CLIConfig{
-		Commands: []*cobra.Command{newSeedCommand()},
-	}
-}
-
-func newSeedCommand() *cobra.Command {
-	return &cobra.Command{
-		Use:   "seed",
-		Short: `Run a "seed node" as a central discovery point for other libp2p nodes`,
-		Args:  cobra.ExactArgs(1),
-
-		RunE: func(cmd *cobra.Command, args []string) error {
-			ctx, cancel := context.WithCancel(cmd.Context())
-			defer cancel()
-
-			h, err := tmlibp2p.NewHost(
-				ctx,
-				tmlibp2p.HostOptions{
-					Options: []libp2p.Option{
-						// Since unspecified, use a dynamic identity and a random listening address.
-						// Specify only tcp for the protocol, just for simplicity in stack traces.
-						libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0"),
-
-						// Unsure if this is something we always want.
-						// Can be controlled by a flag later if undesirable by default.
-						libp2p.ForceReachabilityPublic(),
-					},
-				},
-			)
-			if err != nil {
-				return fmt.Errorf("failed to create libp2p host: %w", err)
-			}
-
-			host := h.Libp2pHost()
-
-			// We currently rely on the libp2p DHT for peer discovery,
-			// so the seed node needs to run the DHT as well.
-			// The validators connect to the DHT through the tmlibp2p.Connection type.
-			if _, err := dht.New(ctx, host, dht.ProtocolPrefix("/gordian")); err != nil {
-				return fmt.Errorf("failed to create DHT peer for seed: %w", err)
-			}
-
-			hostInfo := libp2phost.InfoFromHost(host)
-			p2pAddrs, err := libp2ppeer.AddrInfoToP2pAddrs(hostInfo)
-			if err != nil {
-				return fmt.Errorf("failed to get host p2p addrs: %w", err)
-			}
-
-			var hostAddrs []string
-			for _, a := range p2pAddrs {
-				hostAddrs = append(hostAddrs, a.String())
-			}
-
-			joinedAddrs := strings.Join(hostAddrs, "\n") + "\n" // Trailing newline indicates proper end of input.
-
-			if err := os.WriteFile(args[0], []byte(joinedAddrs), 0600); err != nil {
-				return fmt.Errorf("failed to write seed address output file %q: %w", args[0], err)
-			}
-
-			fmt.Fprintf(cmd.ErrOrStderr(), "Seed running at multiaddrs: %s\n", joinedAddrs)
-
-			return nil
+		Commands: []*cobra.Command{
+			// These commands are all declared in commands.go.
+			newSeedCommand(),
+			newPrintValPubKeyCommand(),
 		},
 	}
 }
