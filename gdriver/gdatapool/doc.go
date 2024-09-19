99d2dafea8e3002f0d7256acdb40bf5076347991
Mark Rushakoff <mark.rushakoff@gmail.com>
2024-09-19 16:23:10 -0400

    chore: remove no-longer-used datapool types
    The attempt to combine providing the data and retrieving it, behind a
    single interface, ended up making it very annoying to use. It has been
    superseded by separate types to independently retrieve catchup block
    data (the catchup client) and to fetch data during voting (the proposed
    block data fetcher).

--- gdriver/gdatapool/doc.go
+++
@@ -1,6 +0,0 @@
-// Package gdatapool contains interfaces for both providing and retrieving block data.
-//
-// From Gordian's perspective, the block data is only a byte slice.
-// It is the driver's responsibility to inform the data pool how to translate
-// the raw bytes into a meaningful value.
-package gdatapool
