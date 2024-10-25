064a46d2e5f8d69020925159297f34d28eec09cf
Alex Cheng <alexander.cheng@utexas.edu>
2024-10-25 12:56:49 -0500

    fix errors in run_gcosmos.sh

--- scripts/run_gcosmos.sh
+++ scripts/run_gcosmos.sh
@@ -12,12 +12,9 @@ go build -o gcosmos .
 
 ./gcosmos init moniker --chain-id=${CHAIN_ID}
 
-# example-mnemonic address: cosmos1r5v5srda7xfth3hn2s26txvrcrntldjumt8mhl
-echo -n "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon art" > example-mnemonic.txt
-
-./gcosmos keys add val --recover --source example-mnemonic.txt
-./gcosmos genesis add-genesis-account val 10000000stake --keyring-backend=test
-./gcosmos genesis gentx val 1000000stake --keyring-backend=test --chain-id=${CHAIN_ID}
+./gcosmos keys add val
+./gcosmos genesis add-genesis-account val 10000000stake
+./gcosmos genesis gentx val 1000000stake --chain-id=${CHAIN_ID}
 ./gcosmos genesis collect-gentxs
 
 ./gcosmos start --g-http-addr 127.0.0.1:$G_HTTP_ADDR --g-grpc-addr 127.0.0.1:$G_GRPC_ADDR
